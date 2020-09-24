709567b463d4987eb52e0267b95fc8fa5333e926
Nicolas Dziuban <nicolas.dziuban@mercadolibre.com>
2020-09-24 12:11:45 -0300

    soapenv

--- src/server.ts
+++ src/server.ts
@@ -583,7 +583,7 @@ export class Server extends EventEmitter {
       const created = getDateString(now);
       const expires = getDateString(new Date(now.getTime() + (1000 * 600)));
 
-      headers += '<o:Security soap:mustUnderstand="1" ' +
+      headers += '<o:Security soapenv:mustUnderstand="1" ' +
         'xmlns:o="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" ' +
         'xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">' +
         '    <u:Timestamp u:Id="_0">' +
